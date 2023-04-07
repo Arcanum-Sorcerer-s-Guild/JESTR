3af9933a3011f41024fd915becbecc909092a30d
Harkerfield <hartsfieldjwh@gmail.com>
2023-04-06 23:49:44 -0800

    modified commits

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const { getUserByLoginName, createUser } = require("./db/controllers/users.js");
 // const { login } = require("./db/controllers/login.js");
 const {
   getListItem,
-  pushListItem,
+  postListItem,
   deleteListItem,
 } = require("./db/controllers/lists.js");
 
@@ -50,36 +50,36 @@ const errorMessage =
 
 // register new user
 app.post("/register", async (req, res) => {
-  const { username, password, isAdmin } = req.body;
+  const { LoginName, Password, IsOwner } = req.body;
 
-  // reject missing username or missing password
-  if (username === "" || password === "") {
+  // reject missing LoginName or missing Password
+  if (LoginName === "" || Password === "") {
     console.log("undefined user or pass");
     return res.status(401).json({
       error: "undefined user or pass",
     });
   }
 
-  // reject duplicate username
-  const duplicate = await getUserByUsername(username);
+  // reject duplicate LoginName
+  const duplicate = await getUserByLoginName(LoginName);
   if (duplicate.length > 0) {
-    console.log("duplicate username of id:", duplicate[0].id);
+    console.log("duplicate LoginName of id:", duplicate[0].id);
     return res.status(401).json({
-      error: "Username already taken...",
+      error: "LoginName already taken...",
     });
   }
 
   try {
-    // hash password and insert user into database
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const data = await createUser(username, hashedPassword, isAdmin);
+    // hash Password and insert user into database
+    const hashedPassword = bcrypt.hashSync(Password, 10);
+    const data = await createUser(LoginName, hashedPassword, IsOwner);
     const user = data[0];
 
     // create session cookie
     req.session.user = {
       userId: user.id,
-      username: user.username,
-      isAdmin: user.is_admin,
+      LoginName: user.LoginName,
+      IsOwner: user.IsOwner,
     };
 
     // send user object to front end for cookie
@@ -94,19 +94,21 @@ app.post("/register", async (req, res) => {
 });
 // login existing user
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+  const { LoginName, Password } = req.body;
 
-  // reject missing username or missing password
-  if (username == undefined || password == undefined) {
+  // reject missing LoginName or missing Password
+  console.log(" Line 101 || app.js || ALERT this log should be here... this is to login without a Password if null");
+  // if (LoginName == undefined || Password == undefined) {
+  if (LoginName == undefined) {
     console.log("undefined user or pass");
     return res.status(401).json({
-      error: "Missing Username or Password",
+      error: "Missing LoginName or Password",
     });
   }
 
   try {
     // search db for user
-    const data = await getUserByUsername(username);
+    const data = await getUserByLoginName(LoginName);
 
     // reject if user not found
     if (data.length === 0) {
@@ -118,20 +120,20 @@ app.post("/login", async (req, res) => {
     const user = data[0];
     console.log("user:", user);
 
-    // compare password hashes and reject if incorrect
-    const matches = bcrypt.compareSync(password, user.password);
+    // compare Password hashes and reject if incorrect
+    const matches = bcrypt.compareSync(Password, user.Password);
     if (!matches) {
-      console.log("incorrect username or password");
+      console.log("incorrect LoginName or Password");
       return res.status(401).json({
-        error: "Incorrect username or password",
+        error: "Incorrect LoginName or Password",
       });
     }
 
     // create session cookie
     req.session.user = {
       userId: user.id,
-      username: user.username,
-      isAdmin: user.is_admin,
+      LoginName: user.LoginName,
+      IsOwner: user.IsOwner,
     };
 
     // send user object to front end for cookie
@@ -169,10 +171,9 @@ app.post("/fetch-user", async (req, res) => {
   return res.sendStatus(403);
 });
 
-app.get("/users", (req, res) => {
-  res.status(200).json(getUsers());
-});
-
+// app.get("/users", (req, res) => {
+//   res.status(200).json(getUsers());
+// });
 
 app.get("/", (req, res) => {
   console.log(app);
@@ -187,33 +188,118 @@ app.get("/test", (req, res) => {
 });
 
 // Get All Items
+//http://localhost:3001/_api/web/lists/GetByTitle('Reservations')/items
+//http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items
 app.get(
   "/_api/web/lists/GetByTitle\\(':listTitle'\\)/items",
   (req, res) => {
-    res.status(200).json("working");
-  }
-);
+    console.log(req.params.listTitle);
+    getListItem(req.params.listTitle)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
+      })
+  });
 
 // Get Single Items
+// http://localhost:3001/_api/web/lists/GetByTitle('Reservations')/items(1)
+// http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items(1)
 app.get(
-  "/_api/web/lists/GetByTitle\\(':listTitle'\\)/items:itemId",
+  "/_api/web/lists/GetByTitle\\(':listTitle'\\)/items\\(:itemId\\)",
   (req, res) => {
-    res.status(200).json("working");
-  }
-);
+    const listLocation = req.params.listTitle;
+    const itemId = req.params.itemId;
+    getListItem(listLocation, itemId)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
+      })
+  });
 
-app.post(
-  "/_api/web/lists/GetByTitle\\(':listTitle'\\)/items/:itemId",
-  (req, res) => {
-    res.status(200).json("working");
-  }
-);
-
+// Post List Item
+//http://localhost:3001/_api/web/lists/GetByTitle('Reservations')/items
+//http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items
 app.post(
   "/_api/web/lists/GetByTitle\\(':listTitle'\\)/items",
   (req, res) => {
-    res.status(200).json("working");
+    const listLocation = req.params.listTitle;
+    const payload = req.body;
+    postListItem(listLocation, payload)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        })
+      });
+  });
+
+app.post(
+  "/_api/web/lists/GetByTitle\\(':listTitle'\\)/items\\(:itemId\\)",
+  (req, res) => {
+    const listLocation = req.params.listTitle;
+    const payload = req.body;
+    const itemId = req.params.itemId;
+    postListItem(listLocation, payload, itemId)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        })
+      });
   }
 );
-
 module.exports = app;
+
+// payload tests
+//Assets
+// [
+//   {
+//     "Serial": "x1y2z3",
+//     "Equipment": "SA-20",
+//     "Threat": "XM-15",
+//     "ThreatType": "Manned",
+//     "SystemInformation": "some string",
+//     "StatusDate": "2022-11-02T19:44:06Z",
+//     "Status": "GREEN",
+//     "ETIC": "2022-11-02T19:44:06Z",
+//     "Remarks": "some string",
+//     "Schedulable": true,
+//     "Operational": true,
+//     "Range": 2205,
+//     "SiteLocation": "string ex Charlie Batt",
+//     "Latitude": "37.220",
+//     "Longitude": "123.456",
+//     "Elevation": 2000,
+//     "Accuracy": "+/- 2m",
+//     "CoordSource": "GARMIN GPX 55I",
+//     "CoordRecordedDate": "2022-11-02T19:44:06Z"
+//   }
+// ]
+// Reservations
+// [
+//   {
+//     "Squadron": "VMGR-152",
+//     "ContactDSN": "123-456-7890",
+//     "Range": 2211,
+//     "SiteLocation": "Charlie batt",
+//     "Threat": "SA-3",
+//     "Equipment": "T-2",
+//     "ThreatType": "unmanned",
+//     "EndDate": "2021-05-18T19:00:00Z",
+//     "EventDate": "2021-05-18T21:00:00Z",
+//     "Notes": "fgdsfjgkfgds",
+//     "Status": "Pending"
+//   }
+// ]
