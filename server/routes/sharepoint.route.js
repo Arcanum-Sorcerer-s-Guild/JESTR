ebd9f715955e3b5e79aa8fb89b4dcf9261839536
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-04-09 15:10:09 -0400

    Correct comments and remove completed testing TODOs

--- server/routes/sharepoint.route.js
+++ server/routes/sharepoint.route.js
@@ -20,7 +20,7 @@ router.get("/GetByTitle\\(':listTitle'\\)/items", (req, res) => {
     });
 });
 
-// Get Single Items
+// Get Single Item
 // http://localhost:3001/_api/web/lists/GetByTitle('Reservations')/items(1)
 // http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items(1)
 router.get("/GetByTitle\\(':listTitle'\\)/items\\(:itemId\\)", (req, res) => {
@@ -44,10 +44,8 @@ router.get("/GetByTitle\\(':listTitle'\\)/items\\(:itemId\\)", (req, res) => {
 });
 
 // Post List Item
-//http://localhost:3001/_api/web/lists/GetByTitle('Reservations')/items
-//http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items
-
-// TODO - Need to check to see if user can edit assets with user authentication
+// http://localhost:3001/_api/web/lists/GetByTitle('Reservations')/items
+// http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items
 router.post("/GetByTitle\\(':listTitle'\\)/items", (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ message: 'unauthorized' });
@@ -74,7 +72,6 @@ router.post("/GetByTitle\\(':listTitle'\\)/items", (req, res) => {
 // Update List Item
 //http://localhost:3001/_api/web/lists/GetByTitle('Reservations')/items(1)
 //http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items(1)
-// TODO - Need to check to see if user can edit assets with user authentication
 router.put("/GetByTitle\\(':listTitle'\\)/items\\(:itemId\\)", (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ message: 'unauthorized' });
@@ -105,7 +102,6 @@ router.put("/GetByTitle\\(':listTitle'\\)/items\\(:itemId\\)", (req, res) => {
 
 // Delete List Item
 //http://localhost:3001/_api/web/lists/GetByTitle('Assets')/items(1)
-// TODO - Need to check to see if user can delete assets with user authentication
 router.delete(
   "/GetByTitle\\(':listTitle'\\)/items\\(:itemId\\)",
   (req, res) => {
@@ -129,45 +125,3 @@ router.delete(
 );
 
 module.exports = router;
-
-// payload tests
-//Assets
-// [
-  // {
-  //   "Serial": "x1y2z3",
-  //   "Equipment": "SA-20",
-  //   "Threat": "XM-15",
-  //   "ThreatType": "Manned",
-  //   "SystemInformation": "some string",
-  //   "StatusDate": "2022-11-02T19:44:06Z",
-  //   "Status": "GREEN",
-  //   "ETIC": "2022-11-02T19:44:06Z",
-  //   "Remarks": "some string",
-  //   "Schedulable": true,
-  //   "Operational": true,
-  //   "Range": 2205,
-  //   "SiteLocation": "string ex Charlie Batt",
-  //   "Latitude": "37.220",
-  //   "Longitude": "123.456",
-  //   "Elevation": 2000,
-  //   "Accuracy": "+/- 2m",
-  //   "CoordSource": "GARMIN GPX 55I",
-  //   "CoordRecordedDate": "2022-11-02T19:44:06Z"
-  // }
-// ]
-// Reservations
-// [
-//   {
-//     "Squadron": "VMGR-152",
-//     "ContactDSN": "123-456-7890",
-//     "Range": 2211,
-//     "SiteLocation": "Charlie batt",
-//     "Threat": "SA-3",
-//     "Equipment": "T-2",
-//     "ThreatType": "unmanned",
-//     "EndDate": "2021-05-18T19:00:00Z",
-//     "EventDate": "2021-05-18T21:00:00Z",
-//     "Notes": "fgdsfjgkfgds",
-//     "Status": "Pending"
-//   }
-// ]
