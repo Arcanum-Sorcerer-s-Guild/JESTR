ddd8064df828adb8f11e2731b9ca78f090ac8c57
ddbonilla <ddbonillav@gmail.com>
2023-04-11 14:59:45 -0700

    login link created

--- client/src/NavBar/NavBar.js
+++ client/src/NavBar/NavBar.js
@@ -12,6 +12,35 @@ const NavBar = () => {
     { name: 'AllReservations', to: '/', text: 'All Reservations', icon: '' },
   ];
 
+  let loginLink = (
+    <Link
+      to="Login"
+      className="block mt-4 lg:inline-block lg:mt-0 hover:text-text p-2"
+    >
+      <span>Login</span>
+    </Link>
+  );
+
+  //Conditional Login button
+  // if(user.username) {
+  //   loginLink = (
+  //     <Link
+  //       to="Login"
+  //       onClick={() => {
+  //         fetch(`${url}/logout`, {
+  //           method: "POST",
+  //           credentials: "include",
+  //         })
+  //         .then(() => {
+  //           //set user({});
+  //         })
+  //       }}
+  //     >
+  //     <span>Logout</span>
+  //     </Link>
+  //   )
+  // }
+
   return (
     <>
       <div className="bg-blue flex items-center justify-between flex-wrap p-4 shadow-md">
@@ -21,15 +50,16 @@ const NavBar = () => {
         <div className="w-full block flex-grow lg:flex lg: items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
             {navLinks.map((link, i) => (
-              <Link 
-                key={i} 
-                to={link.name} 
+              <Link
+                key={i}
+                to={link.name}
                 className="block mt-4 lg:inline-block lg:mt-0 hover:text-text p-2"
               >
                 {link.text}
               </Link>
             ))}
           </div>
+          <div>{loginLink}</div>
         </div>
       </div>
     </>
