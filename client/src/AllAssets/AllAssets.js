a5c89aafa0b53d389550f92bd9f2687cb053882d
hackek <hackett.kd@gmail.com>
2023-04-12 17:59:31 -0600

    Stylized a table to view all assets. Table is currently unsortable.

--- client/src/AllAssets/AllAssets.js
+++ client/src/AllAssets/AllAssets.js
@@ -45,34 +45,84 @@ const AllAssets = () => {
 
   // Formats the list of all assets
   return (
-    <div>
-      {console.log(userData)}
-      <h1>AllAssets</h1>
-      {userData.IsApprover ?
-        <button type="button">Add Asset</button> :
-        <div></div>
-      }
-      <div>
-        {currAssets.map((asset) => {
-          return (
-            <div key={asset.Id}>
-              <div>Site Location: {asset.SiteLocation}</div>
-              <div>Range: {asset.Range}</div>
-              <div>Coordinates: {formatLatLong(asset.Latitude, asset.Longitude)}</div>
-              <div>Bullseye: {asset.Range}</div>
-              <div>Elevation: {asset.Elevation}</div>
-              <div>Serial: {asset.Serial}</div>
-              <div>Threat (Equipment): {asset.Equipment} ({asset.Threat})</div>
-              <br/>
+    <div className="max-w-6xl mx-auto">
+      <div className="mt-4 p-2 rounded-md shadow-md bg-purple text-text text-center">
+          <h3 className="font-semibold">All Assets</h3>
+      </div>
+      <div className>
+        <div className="mt-2">
+          <div className="mt-2 flex flex-col">
+            <div className="my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+              <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="shadow-lg overflow-hidden border-b sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-light/100">
+                    <thead className="bg-gray-light/50 text-gray/100">
+                      <tr>
+                        {columnHeaders.map((header, i) => (
+                          <th
+                            key={i}
+                            className='px-6 py-2 text-center text-xs font-medium uppercase tracking-wider'
+                          >
+                            {header.name}
+                          </th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody className="bg-text divide-y divide-y-gray/75">
+                      {currAssets.map((asset, i) => (
+                        <tr key={i}>
+                          <td className="text-left text-xs">{asset.SiteLocation}</td>
+                          <td className="text-left text-xs">{asset.Range}</td>
+                          <td className="text-left text-xs">{formatLatLong(asset.Latitude, asset.Longitude)}</td>
+                          <td className="text-left text-xs">{asset.Range}</td>
+                          <td className="text-left text-xs">{asset.Elevation}</td>
+                          <td className="text-left text-xs">{asset.Serial}</td>
+                          <td className="text-left text-xs">{asset.Equipment} ({asset.Threat})</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
-          );
-        })}
+          </div>
+        </div>
       </div>
     </div>
+
+
+
+
+
+
   );
 };
 
 
+{/* <div>
+{console.log(userData)}
+<h1>AllAssets</h1>
+{userData.IsApprover ?
+  <button type="button">Add Asset</button> :
+  <div></div>
+}
+<div>
+  {currAssets.map((asset) => {
+    return (
+      <div key={asset.Id}>
+        <div>Site Location: {asset.SiteLocation}</div>
+        <div>Range: {asset.Range}</div>
+        <div>Coordinates: {formatLatLong(asset.Latitude, asset.Longitude)}</div>
+        <div>Bullseye: {asset.Range}</div>
+        <div>Elevation: {asset.Elevation}</div>
+        <div>Serial: {asset.Serial}</div>
+        <div>Threat (Equipment): {asset.Equipment} ({asset.Threat})</div>
+        <br/>
+      </div>
+    );
+  })}
+</div>
+</div> */}
 
 // Exports AllAssets for usability
 export default AllAssets;
