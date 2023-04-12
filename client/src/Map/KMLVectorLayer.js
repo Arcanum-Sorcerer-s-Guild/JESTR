f624db2182282e68a6bd3e6806b853dfabc3fcae
JasonMartin85 <jason.m.martin85@gmail.com>
2023-04-12 14:10:27 -0400

    Minor updates to KML formatting

--- client/src/Map/KMLVectorLayer.js
+++ client/src/Map/KMLVectorLayer.js
@@ -5,32 +5,19 @@ import VectorSource from 'ol/source/Vector.js'
 import OLVectorLayer from 'ol/layer/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import { Vector } from 'ol/layer.js'
-// import VectorSource from 'ol/source/Vector.js'
-
-
 
 
 const KMLVectorLayer = ({source, style, zIndex = 0}) => {
   const {map} = useContext(MapContext)
   useEffect(()=> {
     if (!map) return;
-    // let vectorLayer = new Vector({
-    //   url: './airspace.kml',
-    //   format: new KML(),
-    // })
 
     let vectorLayer = new VectorLayer({
         source: new VectorSource({
           url: 'http://localhost:3000/airspace.kml',
           format: new KML(),
         })
-        // source: new KML({
-        //   url: 'http://localhost:3001/airspace.kml'
-        // })
     })
-
-
-
 
     map.addLayer(vectorLayer);
     vectorLayer.setZIndex(zIndex);
