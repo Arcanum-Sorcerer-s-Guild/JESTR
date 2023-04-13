38f3b834a7f97186a523704027c29fd66fe8d6ef
Kyle Hackett <hackett.kd@gmail.com>
2023-04-12 22:02:36 -0600

    Added end of file line

--- client/src/AllAssets/AllAssets.js
+++ client/src/AllAssets/AllAssets.js
@@ -125,12 +125,3 @@ const AllAssets = () => {
 // Exports AllAssets for usability
 export default AllAssets;
 
-  // // Helper function to convert coordinates from the DD format to the DMS format
-  // const convertDDtoDMS = (coord) => {
-  //   return `${Math.trunc(Math.abs(coord))}\u00B0${Math.trunc(Math.abs(coord)%1*60)}'${((Math.abs(coord)%1*60)%1*60).toFixed(2)}"`;
-  // }
-
-  // // Helper function to display both latitude and longitude in a
-  // const formatLatLong = (lat, long) => {
-  //   return `${lat<0 ? "S": "N"}${convertDDtoDMS(lat)}, ${long<0 ? "W": "E"}${convertDDtoDMS(long)}`;
-  // }
