01573e8cd46efee91e82987f45ab8853409e7884
hackek <hackett.kd@gmail.com>
2023-04-12 21:41:24 -0600

    Took out some unneeded variables and finalized the table sorting for now

--- client/src/AllAssets/TableHead.js
+++ client/src/AllAssets/TableHead.js
@@ -1,10 +1,10 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { AssetsContext } from './AllAssets';
 
 const TableHead = ({ columns, handleSorting }) => {
   const { currAssets, setCurrAssets } = React.useContext(AssetsContext);
-  const [sortField, setSortField] = useState("SiteLocation");
+  const [sortField, setSortField] = useState("");
   const [order, setOrder] = useState("asc");
 
   const handleSortingChange = (accessor) => {
