01da5f719a6d6b6605a72b1527f209cd8176371c
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-04-14 14:25:14 -0400

    Fix merge conflict errors

--- server/db/controllers/users.js
+++ server/db/controllers/users.js
@@ -6,11 +6,9 @@ const getAllUsers = async () => {
 
 const getUserById = async (userId) => {
   return await knex('Users').where('Id', '=', userId);
-  return await knex('Users').where('Id', '=', userId);
 };
 
 const getUserByEmail = async (email) => {
-  return await knex('Users').where('Email', 'ilike', email);
   return await knex('Users').where('Email', 'ilike', email);
 };
 
