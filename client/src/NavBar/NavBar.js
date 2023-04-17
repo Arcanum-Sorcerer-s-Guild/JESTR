59b4eb326c86d4306a4ce0b84f0578572e336f47
ddbonilla <ddbonillav@gmail.com>
2023-04-17 09:49:18 -0700

    nav responsive draft

--- client/src/NavBar/NavBar.js
+++ client/src/NavBar/NavBar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Context } from '../App';
 
 const NavBar = () => {
   const { userData, setUserdata } = React.useContext(Context);
   const navigate = useNavigate();
+  const [open, setOpen]= useState(true)
+
   const navLinks = [
     { name: 'QuickLook', to: '/', text: 'Quick Look', icon: '' },
     { name: 'AllAssets', to: '/', text: 'All Assets', icon: '' },
@@ -16,13 +18,13 @@ const NavBar = () => {
     <>
       <Link
         to="Login"
-        className="block mt-4 lg:inline-block lg:mt-0 hover:text-text p-2"
+        className={`${!open && "hidden"} duration-300 flex md:inline-flex p-4 items-center bg-pink/30 md:bg-gunmetal hover:bg-gray-light`}
       >
         <span>Login</span>
       </Link>
       <Link
         to="Register"
-        className="block mt-4 lg:inline-block lg:mt-0 hover:text-text p-2"
+        className={`${!open && "hidden"} duration-300 flex md:inline-flex p-4 items-center bg-pink/30 md:bg-gunmetal hover:bg-gray-light`}
       >
         <span onClick={() => setUserdata({})}>Register</span>
       </Link>
@@ -49,35 +51,54 @@ const NavBar = () => {
   let loggedIn = (
     <Link
       to="Login"
-      className="block mt-4 lg:inline-block lg:mt-0 hover:text-text p-2"
+      className={`${!open && "hidden"} duration-300 flex md:inline-flex p-4 items-center bg-gunmetal hover:bg-gray-light text-green`}
+      
     >
       <span onClick={signOut}>Signout</span>
     </Link>
   );
 
-  //Conditional Login button
-  // if(!("Id in userData")) {
-  //   loginLink = (
-  //     <Link
-  //       to="Login"
-  //       onClick={() => {
-  //         fetch(`${url}/logout`, {
-  //           method: "POST",
-  //           credentials: "include",
-  //         })
-  //         .then(() => {
-  //           //set user({});
-  //         })
-  //       }}
-  //     >
-  //     <span>Logout</span>
-  //     </Link>
-  //   )
-  // }
-
   return (
     <>
-      <div className="bg-gunmetal text-pink flex items-center justify-between flex-wrap p-4 shadow-md shadow-pink/50">
+      <div className="flex px-4 border-b shadow-md shadow-pink/100 items-center relative bg-gunmetal">
+        <div className="text-lg font-bold md:py-0 py-4">
+          <span className="text-blue">JESTER</span>
+        </div>
+        <div className="md:px-2 ml-auto md:flex md:space-x-2 absolute md:relative top-full 
+        left-0 right-0 bg-pink/30 md:bg-gunmetal text-pink text-sm">
+          <div className={`${!open && "hidden"} duration-300`}>
+            {navLinks.map((link, i) => (
+              <Link
+                key={i}
+                to={link.name}
+                className="flex md:inline-flex p-4 items-center hover:bg-gray-light"
+              >
+                {link.text}
+              </Link>
+            ))}
+            <Link
+              className={`${userData.IsSiteAdmin === true ? '' : 'hidden'} flex md:inline-flex p-4 items-center hover:bg-gray-light`}
+              to="/Admin"
+            >
+              Admin
+            </Link>
+          </div>
+          {'Id' in userData ? <div>{loggedIn}</div> : <div>{notLoggedIn}</div>}
+        </div>
+        <div className='ml-auto md:hidden cursor-pointer'>
+          <span 
+            className='w-5 h-5 fill-current'
+            onClick={() => setOpen(!open)}
+          >X</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
+
+{/* <div className="bg-gunmetal text-pink flex items-center justify-between flex-wrap p-4 shadow-md shadow-pink/50">
         <div className="flex items-center flex-shrink-0 text-text mr-6">
           <span className="font-semibold text-xl text-blue tracking-tight">JESTER</span>
         </div>
@@ -101,9 +122,4 @@ const NavBar = () => {
           </div>
           {'Id' in userData ? <div>{loggedIn}</div> : <div>{notLoggedIn}</div>}
         </div>
-      </div>
-    </>
-  );
-};
-
-export default NavBar;
+      </div> */}
