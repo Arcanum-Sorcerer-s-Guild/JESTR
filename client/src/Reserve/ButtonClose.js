ef7473424e75b592d7dad51b02322cef6841c3bf
Harkerfield <hartsfieldjwh@gmail.com>
2023-04-18 23:20:19 -0800

    Added button component with class

--- client/src/Reserve/ButtonClose.js
+++ client/src/Reserve/ButtonClose.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function ButtonClose({ name, onClick }) {
     return (
         <button
-            className="h-6 w-auto m-2 text=xxl text-center border rounded content-center  bg-primary"
+            className="h-6 w-6 m-2 text=xxl text-center border rounded content-center  bg-primary"
             onClick={() => onClick()}
         >
             {name}
