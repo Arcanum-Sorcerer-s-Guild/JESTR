2e2841fc947fecacf9b81aecbbba9816dce7e754
Harkerfield <hartsfieldjwh@gmail.com>
2023-04-18 23:23:32 -0800

    updated css style

--- client/src/Reserve/UserDropDown.js
+++ client/src/Reserve/UserDropDown.js
@@ -14,7 +14,7 @@ const UserDropDown = ({ dropDownItems, dropdown, setDropdown }) => {
             <span className="block truncate">{dropdown}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
-                className="h-5 w-5 text-gray-400"
+                className="h-auto w-5 text-gray-400"
                 aria-hidden="true"
               />
             </span>
